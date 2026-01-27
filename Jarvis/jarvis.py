d43e6a063914ff1b8ddb49fa3f172c078c08c6bc
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-27 09:35:13 +0530

    Update jarvis.py

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -9,7 +9,7 @@ import pyautogui
 import pyjokes   
      
 engine = pyttsx3.init()   
-voices = engine.getProperty('voices')  
+voices = engine.getProperty('voices')   
 engine.setProperty('voice', voices[1].id)   
 engine.setProperty('rate', 150)
 engine.setProperty('volume', 1)
