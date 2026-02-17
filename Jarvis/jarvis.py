9eb654232d94ff7ea06a1a6f35131c85dd5468f8
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-17 17:03:48 +0530

    Update jarvis.py

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -7,7 +7,7 @@ import os
 import random               
 import pyautogui                
 import pyjokes        
-         
+          
 engine = pyttsx3.init()    
 voices = engine.getProperty('voices')    
 engine.setProperty('voice', voices[1].id)   
